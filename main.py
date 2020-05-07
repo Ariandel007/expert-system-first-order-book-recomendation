4dcfb3db148ea16b29f4e8c4169116848c855aa4
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2020-05-07 15:09:52 -0500

    condes and classes added

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ api = Api(app)
 
 #-----------------------------------LOGICA FIRST ORDER------------------------------------------------------------------
 
-class Libro:
-    def __init__(self, nombre):
-        self.nombre = nombre
-
 archBooks = pd.read_csv('books.csv')
 archBooks_tags = pd.read_csv('book_tags.csv')
 
@@ -49,92 +45,107 @@ user_id = archRatings['user_id'].values
 book_id_rating = archRatings['book_id'].values
 rating = archRatings['rating'].values
 
+#-------------------------------Clase Libro-----------------------------------------------------------------------------
 
+class Libro:
+    def __init__(self, nombre):
+        self.nombre = nombre
 
-#Base del conocimiento chidori
+#--------------------------------clase sistema experto------------------------------------------------------------------
+class SistemaExperto:
+    def __init__(self):
+        # Base del conocimiento
+        self.usuario = var()
+        self.libro = var()
+        self.libro_goodread = var()
+        self.autores = var()
+        self.titulo_libro = var()
+        self.tag = var()
+        self.tag_nombre = var()
+        self.rating_rating = var()
 
-usuario = var()
-libro = var()
-libro_goodread = var()
-autores = var()
-titulo_libro = var()
-tag = var()
-tag_nombre = var()
-rating_rating = var()
+        self.Book = Relation()
+        self.BookTags = Relation()
+        self.Tags = Relation()
+        self.Ratings = Relation()
 
-Book = Relation()
-BookTags = Relation()
-Tags = Relation()
-Ratings = Relation()
+        for i in range(len(book_id_book)):
+            fact(self.Book, book_id_book[i], title_book[i], goodreads_book_id_book[i])
 
-for i in range(len(book_id_book)):
-    fact(Book, book_id_book[i], title_book[i], goodreads_book_id_book[i])
+        for i in range(len(goodreads_book_id_book_tags)):
+            fact(self.BookTags, goodreads_book_id_book_tags[i], tag_id_book_tags[i])
 
-for i in range(len(goodreads_book_id_book_tags)):
-    fact(BookTags, goodreads_book_id_book_tags[i], tag_id_book_tags[i])
+        for i in range(len(tag_id_tags)):
+            fact(self.Tags, tag_id_tags[i], tag_name[i])
 
-for i in range(len(tag_id_tags)):
-    fact(Tags, tag_id_tags[i], tag_name[i])
+        for i in range(len(rating)):
+            fact(self.Ratings, user_id[i], book_id_rating[i], rating[i])
 
-for i in range(len(rating)):
-    fact(Ratings, user_id[i], book_id_rating[i], rating[i])
+    #
+    def leyoEstosLibro(self, x, y):
+        return conde((self.Ratings(x, self.libro, self.rating_rating), self.Book(self.libro, self.titulo_libro, y)))
 
+    # Igual que el de arriba solo que devolvera el titulo
+    def leyoEstosLibrosTitulo(self, x, y):
+        return conde((self.Ratings(x, self.libro, self.rating_rating), self.Book(self.libro, y, self.libro_goodread)))
 
-def recomendar_lista_libros(usuarioID):
-    resultado_libros_usuario = run(0, (libro_goodread), Ratings(usuarioID, libro, rating_rating),
-                                   Book(libro, titulo_libro, libro_goodread))
+    def esteLibroTieneEstosTags(self, x, y):
+        return conde((self.Book(self.libro, self.titulo_libro, x), self.BookTags(x, y)))
 
-    resultado_libros_usuario_nombre = run(0, (titulo_libro), Ratings(usuarioID, libro, rating_rating),
-                                          Book(libro, titulo_libro, libro_goodread))
+    def estosTagsEstanPresentesEnEstosLibros(self, x, y):
+        return conde((self.BookTags(self.libro_goodread, y), self.Book(self.libro, x, self.libro_goodread)))
 
-    # BookTags(libro_goodread, tag)
-    resultado_tags_usuario = ()
+    def recomendar_lista_libros(self, usuarioID):
+        resultado_libros_usuario = run(0, self.libro_goodread, self.leyoEstosLibro(usuarioID, self.libro_goodread))
 
-    for x in resultado_libros_usuario:
-        resultado_tags_usuario = resultado_tags_usuario + run(0, (tag), Book(libro, titulo_libro, x), BookTags(x, tag))
+        resultado_libros_usuario_nombre = run(0, self.titulo_libro, self.leyoEstosLibrosTitulo(usuarioID, self.titulo_libro))
 
-    # eliminacion de duplicados
-    lista_tags = list(resultado_tags_usuario)
-    lista_tags = list(dict.fromkeys(lista_tags))
+        # BookTags(libro_goodread, tag)
+        resultado_tags_usuario = ()
 
-    resultado = ()
+        for x in resultado_libros_usuario:
+            resultado_tags_usuario = resultado_tags_usuario + run(0, self.tag, self.esteLibroTieneEstosTags(x, self.tag))
 
-    if (len(lista_tags) < 10):
-        for y in lista_tags:
-            resultado = resultado + run(0, (titulo_libro), BookTags(libro_goodread, y),
-                                        Book(libro, titulo_libro, libro_goodread))
+        # eliminacion de duplicados
+        lista_tags = list(resultado_tags_usuario)
+        lista_tags = list(dict.fromkeys(lista_tags))
 
-    else:
-        for i in range(10):
-            resultado = resultado + run(0, (titulo_libro), BookTags(libro_goodread, lista_tags[i]),
-                                        Book(libro, titulo_libro, libro_goodread))
+        resultado = ()
 
-    # eliminacion de duplicados
-    lst1 = list(resultado)
-    lst1 = list(dict.fromkeys(lst1))
+        if len(lista_tags) < 10:
+            for y in lista_tags:
+                resultado = resultado + run(0, self.titulo_libro, self.estosTagsEstanPresentesEnEstosLibros(self.titulo_libro, y))
+        else:
+            for i in range(10):
+                resultado = resultado + run(0, self.titulo_libro, self.estosTagsEstanPresentesEnEstosLibros(self.titulo_libro, lista_tags[i]))
 
-    # eliminacion de ya leidos
-    for i in range(len(resultado_libros_usuario_nombre)):
-        if resultado_libros_usuario_nombre[i] in lst1:
-            lst1.remove(resultado_libros_usuario_nombre[i])
+        # eliminacion de duplicados
+        lst1 = list(resultado)
+        lst1 = list(dict.fromkeys(lst1))
 
-    lst_final = []
+        # eliminacion de ya leidos
+        for i in range(len(resultado_libros_usuario_nombre)):
+            if resultado_libros_usuario_nombre[i] in lst1:
+                lst1.remove(resultado_libros_usuario_nombre[i])
 
-    for i in range(len(lst1)):
-        lst_final.append(Libro(lst1[i]))
+        lst_final = []
 
-    return lst_final
+        for i in range(len(lst1)):
+            lst_final.append(Libro(lst1[i]))
 
-def libros_leidos(usuarioID):
-    resultado_libros_usuario_nombre = run(0, (titulo_libro), Ratings(usuarioID, libro, rating_rating), Book(libro, titulo_libro, libro_goodread))
-    lista_leidos = list(resultado_libros_usuario_nombre)
+        return lst_final
 
-    lst_leidos = []
+    def retornar_libros_leidos(self, usuarioID):
+        resultado_libros_usuario_nombre = run(0, self.titulo_libro, self.leyoEstosLibrosTitulo(usuarioID, self.titulo_libro))
 
-    for i in range(len(lista_leidos)):
-        lst_leidos.append(Libro(lista_leidos[i]))
+        lst_leidos = []
 
-    return lst_leidos
+        for i in range(len(resultado_libros_usuario_nombre)):
+            lst_leidos.append(Libro(resultado_libros_usuario_nombre[i]))
+
+        #lst_leidos = list(resultado_libros_usuario_nombre)
+        return lst_leidos
+
 
 #-----------------------------------API FLASK---------------------------------------------------------------------------
 def to_dict(obj):
@@ -150,7 +161,9 @@ class UserResource(Resource):
     def post(self):
         userID = request.json['userID']
 
-        lista_recomendados = recomendar_lista_libros(int(userID))
+        se = SistemaExperto()
+
+        lista_recomendados = se.recomendar_lista_libros(int(userID))
 
         lst.clear()
 
@@ -171,7 +184,9 @@ class LibrosleidosResource(Resource):
     def post(self):
         userID = request.json['userID']
 
-        lista_leidos = libros_leidos(int(userID))
+        se = SistemaExperto()
+
+        lista_leidos = se.retornar_libros_leidos(int(userID))
 
         lst2.clear()
 
